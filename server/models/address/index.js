83dcee12da561449cd7f41bbece1b49f950be0e6
Dionisio Ripoll <ripoll.dioni@gmail.com>
2016-02-20 17:25:33 -0400

    Finishing User and Address models

---
+++ server/models/address/index.js
@@ -0,0 +1,13 @@
+import { Schema } from 'mongoose';
+
+const AddressSchema = new Schema({
+  street: String,
+  country: String,
+  city: String,
+  zip: String,
+  state: String,
+  line1: String,
+  line2: String
+});
+
+export default mongoose.model('Address', AddressSchema);
--- server/models/user/index.js
+++ server/models/user/index.js
@@ -1,4 +1,5 @@
 import { Schema } from 'mongoose';
+import { Address } from '../Address/index';
 
 const phoneTransformer = {
   type: String,
@@ -20,6 +21,11 @@ const UserSchema = new Schema({
   profilePic: String,
   description: String,
   rating: Number,
+  address: {
+    type: Schema.ObjectId,
+    require: true,
+    ref: 'Address'
+  },
   phone1: phoneTransformer,
   phone2: phoneTransformer,
   minpph: Number,
@@ -34,5 +40,10 @@ UserSchema
   .get(function() {
     return `${this.firstName} ${this.lastName}`;
   });
+
+UserSchema.pre('save', function() {
+  Object.assign(this, { address : this.address || new Address() });
+  return this.address.save instanceof Function ? this.address.save(next) : next();
+});
 
 export default mongoose.model('User', UserSchema);
