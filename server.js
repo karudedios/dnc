a26272f4ec6cd6b1222da85c048f8255d5e51739
Dionisio Ripoll <ripoll.dioni@gmail.com>
2016-02-20 13:54:42 -0400

    Initial Server structure

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
-var express = require('express');
+import http from 'http';
+import path from 'path';
+import express from 'express';
+import mongoose from 'mongoose';
+import socketio from 'socket.io';
+//import api from './sever/routes';
+import bodyParser from 'body-parser';
 
-var app = express();
+const app = express();
+const server = http.createServer(app);
 
-app.listen(3000);
+const io = socketio(server);
+
+//const router = express.Router().use('/api', api);
+
+//app.use(router);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  const addr = server.address();
+  console.log(`Server running ${addr.address}:${addr.port}`);
+});
+
+const onConnect = (socket) => {
+
+};
+
+io.on('connect', onConnect);
